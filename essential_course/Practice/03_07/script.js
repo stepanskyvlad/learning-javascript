c4704d8596c7cd64e4e5c7811758c150e0ec9f5c
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-11-04 09:00:01 +0200

    solved a practical task

--- essential_course/Practice/03_07/script.js
+++ essential_course/Practice/03_07/script.js
@@ -4,6 +4,58 @@
  * - Create JavaScript objects based on objects in your current environment.
  * - Give each object an identifiable name.
  * - Create properties to describe the objects and set their values.
- * - Find an object that has another object inside of it to create a nested object.
+ * - Find an object that has another object inside it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const myBag = {
+    name: "Study Bag",
+    volume: 28,
+    color: "darkblue",
+    pocketNum: 4,
+    strapLength: {
+      left: 30,
+      right: 30,
+    },
+    isOpen: true,
+    openBag: function (bagStatus) {
+        this.isOpen = bagStatus;
+    },
+    changeStrapLength: function (lengthLeft, lengthRight) {
+        this.strapLength.left = lengthLeft;
+        this.strapLength.right = lengthRight;
+    },
+    book: {
+        name: "Lord of the Rings",
+        color: "green",
+        pages: 800,
+        isRead: false,
+        readBook: function (finishBook) {
+            this.isRead = finishBook
+        },
+    },
+};
+console.log("My bag is:", myBag);
+console.log("The number of pages of the book:", myBag.book.pages);
+
+console.log("Is the book read?", myBag.book.isRead);
+console.log("Reading the book...", myBag.book.name);
+myBag.book.readBook(true);
+console.log("Is the book read?", myBag.book.isRead);
+
+console.log("Name of my bag:", myBag.name, "\nColor:", myBag.color,
+    "\nVolume:", myBag.volume);
+
+console.log("Is the bag open?", myBag.isOpen)
+console.log("Closing the bag...")
+myBag.openBag(false)
+console.log("Is the bag open?", myBag.isOpen)
+
+console.log("Length of the straps:", "\nleft strap:",
+    myBag.strapLength.left, "\nright strap:", myBag.strapLength.right);
+
+let lengthLeftStrap = 42;
+let lengthRightStrap = 55;
+
+myBag.changeStrapLength(lengthLeftStrap, lengthRightStrap);
+console.log("Length of the straps:", "\nleft strap:",
+    myBag.strapLength.left, "\nright strap:", myBag.strapLength.right);
