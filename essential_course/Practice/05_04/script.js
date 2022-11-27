9c065efed1865f241830c0e6621b3b750d2240ee
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-11-27 18:20:18 +0200

    updated chapter5

--- essential_course/Practice/05_04/script.js
+++ essential_course/Practice/05_04/script.js
@@ -2,3 +2,9 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+document.querySelectorAll("li:last-child span").forEach(item => item.style.backgroundColor="red");
+document.querySelectorAll("#pack02 li:last-child").forEach(item =>
+    item.style.backgroundColor="lightgrey");
+document.querySelectorAll("#pack02 h1").forEach(item => item.style.backgroundColor="green");
+console.log(document.querySelector("#pack02"))
+console.log(document.querySelector("main li:first-child"))
--- essential_course/Practice/05_08/script.js
+++ essential_course/Practice/05_08/script.js
@@ -3,3 +3,25 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+console.log(document.querySelector("main p"))
+console.log(document.querySelector("main p").className)
+console.log(document.querySelector("main p").className = "description-of-bag")
+console.log(document.querySelector("main p").classList)
+document.querySelector("main p").classList.add("new-class")
+document.querySelector("main p").classList.add("unnecessary-class")
+console.log(document.querySelector("main p").classList)
+document.querySelector("main p").classList.remove("unnecessary-class")
+console.log(document.querySelector("main p").classList)
+console.log(document.querySelector("main p").classList.toggle("unnecessary-class"))
+console.log(document.querySelector("main p").classList.toggle("unnecessary-class"))
+console.log(document.querySelector("main p").classList.toggle("new-class"))
+console.log(document.querySelector("main p").hasAttribute("class"))
+document.querySelector("main p").setAttribute("title", "unnecessary value")
+console.log(document.querySelector("main p").attributes)
+document.querySelector("main p").removeAttribute("title")
+console.log(document.querySelector("main p").style)
+document.querySelector("main p").style.color = "lightgrey"
+document.querySelector("main p").style.backgroundColor = "darkred"
+document.getElementById("description-of-bag").style.borderWidth = "1px 5px 10px 20px";
+document.querySelector("main p").style.borderColor = "darkblue"
+
