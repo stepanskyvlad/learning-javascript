7bed2978ffa0f0923e036d051e2ba05c939f0f3a
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-11-28 19:07:02 +0200

    update chapter6

--- essential_course/06_08/script.js
+++ essential_course/06_08/script.js
@@ -3,14 +3,22 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Arithmetic_operators
  */
 
-let a = 5;
-let b = 4;
+let a = 15;
+let b = "4";
 let c = 3.2;
 
 console.log(`let a: ${a} (${typeof a})`);
 console.log(`let b: ${b} (${typeof b})`);
 console.log(`let c: ${c} (${typeof c})`);
 
-let result = a + b;
+let result1 = a - b;
+console.log("Result: ", result1, typeof result1);
 
-console.log("Result: ", result);
+let result2 = a + b;
+console.log("Result: ", result2, typeof result2);
+
+let result3 = a * b;
+console.log("Result: ", result3, typeof result3);
+
+let result4 = a / b;
+console.log("Result: ", result4, typeof result4);
