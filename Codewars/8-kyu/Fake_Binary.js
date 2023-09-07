42507b070339a2195a41db588dadc57ac3fd6b8e
Vladyslav Yarema <vlad14yarema@gmail.com>
2023-09-08 00:09:18 +0300

    edited the solved task from codewars

--- Codewars/8-kyu/Fake_Binary.js
+++ Codewars/8-kyu/Fake_Binary.js
@@ -4,7 +4,12 @@ function fakeBin(x){
     let result = []
     for (const number of x) {
         if ((number/1) < 5) {
-
+            result.push(0)
+        } else {
+            result.push(1)
         }
     }
+    return result.join("")
 }
+
+console.log(fakeBin('45385593107843568'))
